9039495e9f7872a4caa07596a5b2e6a968dfab26
JVictorIFCE <joaovictorclaudio58@gmail.com>
2025-12-03 14:37:17 -0300

    projeto inicial

---
+++ app/src/main/java/com/example/trabalhofinaln2/LoginViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.trabalhofinaln2
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginViewModel: ViewModel() {
+
+    val auth = FirebaseAuth.getInstance()
+    var logando by mutableStateOf(false)
+
+    var loginObserver: (Boolean) -> Unit = {}
+
+    fun setObserver(localObserver: (Boolean) -> Unit){
+        loginObserver = localObserver
+    }
+
+    fun logar(email: String, senha: String){
+        logando = true
+        auth.signInWithEmailAndPassword(email, senha)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){ //login ok
+                    loginObserver(true)
+                }else {
+                    loginObserver(false)
+                    Log.i("###", task.exception?.message.toString())
+                }
+
+                logando = false
+            }
+    }
+
+    fun cadastrar(email: String, senha: String){
+        logando = true
+        auth.createUserWithEmailAndPassword(email, senha)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){ //login ok
+                    loginObserver(true)
+                }else
+                    loginObserver(false)
+
+                logando = false
+            }
+    }
+
+
+}
